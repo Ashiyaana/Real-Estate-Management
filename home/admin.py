b22bec29127e91f12c5dfd455161ba803a65076e
Ashiyaana <mca117rishika@gmail.com>
2023-05-23 09:45:07 +0530

    initial commit

---
+++ home/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from home.models import*
+# Register your models here.
+@admin.register(contact)
+class contactadmin(admin.ModelAdmin):
+    list_display=('name','email','phone','subject','message',)
+
+@admin.register(properties)
+class productadmin(admin.ModelAdmin):
+    pass
+@admin.register(blog)
+class blogadmin(admin.ModelAdmin):
+    pass
+@admin.register(faq)
+class faqadmin(admin.ModelAdmin):
+    pass
+@admin.register(contactus)
+class contactusadmin(admin.ModelAdmin):
+    list_display=('name','email','phone_no','message','propertyid','userid',)
+
+
